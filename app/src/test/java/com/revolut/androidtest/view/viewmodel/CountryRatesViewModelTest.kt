e498823b48bfcc48beecd0f1880c83c9131ddffc
Ananth <anan.busyguy007@gmail.com>
2020-01-23 00:15:00 +0100

    Should call getRates service on fragment loaded

--- app/src/test/java/com/revolut/androidtest/view/viewmodel/CountryRatesViewModelTest.kt
+++ app/src/test/java/com/revolut/androidtest/view/viewmodel/CountryRatesViewModelTest.kt
@@ -1,13 +1,31 @@
 package com.revolut.androidtest.view.viewmodel
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.nhaarman.mockitokotlin2.verify
+import com.revolut.androidtest.RxTrampolineSchedulerRule
+import com.revolut.androidtest.domain.Country
 import com.revolut.androidtest.domain.RateRepository
+import com.revolut.androidtest.domain.Rates
+import io.reactivex.Single
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
 
-
+@RunWith(MockitoJUnitRunner::class)
 class CountryRatesViewModelTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Rule
+    @JvmField
+    var testSchedulerRule = RxTrampolineSchedulerRule()
+
+
     @Mock
     private lateinit var service: RateRepository
 
@@ -20,8 +38,16 @@ class CountryRatesViewModelTest {
 
     @Test
     fun `should call getRates service on fragment loaded`() {
+        Mockito.`when`(service.getRates()).thenReturn(Single.just(aDummyRates()))
         viewModel.fetchRates()
 
         verify(service).getRates()
+    }
+
+    private fun aDummyRates(): Rates {
+        val countryRates = ArrayList<Country>()
+        countryRates.add(Country("INR", 85.33f))
+        countryRates.add(Country("USA", 1f))
+        return Rates(countryRates, "EUR", "2020-11-01")
     }
 }
