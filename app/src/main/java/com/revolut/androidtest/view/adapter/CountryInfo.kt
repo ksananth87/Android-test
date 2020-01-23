9dab4efff0316517394dee168afdb9555a3c258a
Ananth <anan.busyguy007@gmail.com>
2020-01-23 02:35:39 +0100

    Add enum class for currency full name and icon

---
+++ app/src/main/java/com/revolut/androidtest/view/adapter/CountryInfo.kt
@@ -0,0 +1,39 @@
+package com.revolut.androidtest.view.adapter
+
+import com.revolut.androidtest.R
+
+
+enum class CountryInfo(val countryIcon: Int, val countryFullName: String) {
+    AUD(R.drawable.aud, "Australian Dollar"),
+    BGN(R.drawable.bgn, "Bulgarian lev"),
+    BRL(R.drawable.brl, "Brazilian real"),
+    CAD(R.drawable.cad, "Canadian dollar"),
+    CHF(R.drawable.chf, "Swiss franc"),
+    CNY(R.drawable.cny, "Renminbi"),
+    CZK(R.drawable.czk, "Czech koruna"),
+    DKK(R.drawable.dkk, "Danish krone"),
+    GBP(R.drawable.gbp, "Pound sterling"),
+    HKD(R.drawable.hkd, "Hong Kong dollar"),
+    HRK(R.drawable.hrk, "Croatian kuna"),
+    HUF(R.drawable.huf, "Hungarian forint"),
+    IDR(R.drawable.idr, "Indonesian rupiah"),
+    ILS(R.drawable.ils, "AIsraeli new shekel"),
+    INR(R.drawable.inr, "Indian rupee"),
+    ISK(R.drawable.isk, "Icelandic króna"),
+    JPY(R.drawable.jpy, "Japanese yen"),
+    KRW(R.drawable.krw, "South Korean won"),
+    MXN(R.drawable.mxn, "Mexican peso"),
+    MYR(R.drawable.myr, "Malaysian ringgit"),
+    NOK(R.drawable.nok, "Norwegian krone"),
+    NZD(R.drawable.nzd, "New Zealand dollar"),
+    PHP(R.drawable.php, "Philippine peso"),
+    PLN(R.drawable.pln, "Polish złoty"),
+    RON(R.drawable.ron, "Romanian leu"),
+    RUB(R.drawable.rub, "Russian ruble"),
+    SEK(R.drawable.sek, "Swedish krona"),
+    SGD(R.drawable.sgd, "Singapore dollar"),
+    THB(R.drawable.thb, "Thai baht"),
+    TRY(R.drawable.tru, "Turkish lira"),
+    USD(R.drawable.usd, "United States Dollar"),
+    ZAR(R.drawable.zar, "South African rand")
+}
