9dd2e8ee03d6f206ea149a97999e5e26e774bd3c
Ananth <anan.busyguy007@gmail.com>
2020-01-23 01:02:38 +0100

    Add observer for showing progress

--- app/src/main/java/com/revolut/androidtest/view/fragment/CountryRatesFragment.kt
+++ app/src/main/java/com/revolut/androidtest/view/fragment/CountryRatesFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.revolut.androidtest.CountryRatesModule
 import com.revolut.androidtest.R
 import com.revolut.androidtest.api.APIClient
@@ -19,7 +20,9 @@ class CountryRatesFragment : Fragment() {
     override fun onAttach(context: Context) {
         super.onAttach(context)
         viewModel = CountryRatesModule.create(APIClient().getClient()).getViewModelFor(this, CountryRatesViewModel::class.java)
-
+        viewModel.showProgressDialog().observe(this, Observer {
+            updateSwipeRefresh(it)
+        })
     }
 
     override fun onCreateView(
@@ -29,19 +32,16 @@ class CountryRatesFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_country_rates, container, false)
 
+        viewModel.fetchRates()
+
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initiateSwipeRefresh()
     }
 
-    private fun initiateSwipeRefresh() {
-        swipeRefreshLayout.setOnRefreshListener {
-            swipeRefreshLayout.isRefreshing = false
-            viewModel.fetchRates()
-        }
-        swipeRefreshLayout.isRefreshing = true
+    private fun updateSwipeRefresh(progressStatus: Boolean) {
+        swipeRefreshLayout.isRefreshing = progressStatus
     }
 }
