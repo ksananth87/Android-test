b5f296b4baa93d56450ffc7f2fd24fb861bf89c7
Ananth <anan.busyguy007@gmail.com>
2020-01-26 13:12:42 +0100

    Fix test case

--- app/src/main/java/com/revolut/androidtest/view/utils/CurrencyConverter.kt
+++ app/src/main/java/com/revolut/androidtest/view/utils/CurrencyConverter.kt
@@ -1,11 +1,19 @@
 package com.revolut.androidtest.view.utils
 
+import java.math.BigDecimal
+
 class CurrencyConverter(private val enteredValue: Float, private val fromCurrencyRate: Float?) {
     fun convertTo(toCurrencyRate: Float): Float {
         if(fromCurrencyRate == null){
             return toCurrencyRate
         }
         val updatedAmount = (toCurrencyRate * enteredValue) / fromCurrencyRate
-        return "%.2f".format(updatedAmount).toFloat()
+        return round(updatedAmount,2)
+    }
+
+    private fun round(d: Float, decimalPlace: Int): Float {
+        var bd = BigDecimal(d.toString())
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP)
+        return bd.toFloat()
     }
 }
